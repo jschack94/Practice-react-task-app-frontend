d169778bd0d6351bf14fa3b8c5fea98311cb107c
jschack94 <jschack94@gmail.com>
2020-04-21 18:53:41 -0500

    combine reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,10 @@
+export default (state = null, action) => {
+    switch (action.type) {
+        case 'LOGIN_USER':
+            return action.user
+        case 'LOGOUT_USER':
+            return null
+        default:
+            return state
+    }
+}
